cafa735577c322a4218c941bb555eae80bac15ab
MobDe <MobDevX.outlook.com>
2024-02-14 11:08:35 +0700

    Start WeWatch

---
+++ app/src/main/java/com/sample/watch/SeachActivity.kt
@@ -0,0 +1,11 @@
+package com.sample.watch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeachActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seach)
+    }
+}
